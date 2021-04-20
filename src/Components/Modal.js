85c0076af66dd182e9775a3e50256f4e9283a40f
Samatar Xasan <Samatar.Xasan@gmail.com>
2021-04-20 13:02:59 +0100

    Modal updates

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -5,19 +5,22 @@ function MyVerticallyCenteredModal(props) {
   return (
     <Modal
       {...props}
-      size="lg"
+      size="md"
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Sign up to our weekly Newsletter
+      <Modal.Header  closeButton >
+        <Modal.Title  id="contained-modal-title-vcenter">
+          <h4>Sign up to our weekly Newsletter</h4>
         </Modal.Title>
       </Modal.Header>
-      <Modal.Body>
-        <input type="email" required/>
-        <Button onClick={props.onHide}>Submit</Button>
+      <Modal.Body style={{display:'flex',justifyContent:'center', alignItems:'center'}}>
+        <input type="email" required style={{width:'200px'}} />
+        <Button onClick={props.onHide}  style={{margin:'0px 5px', padding:'3px 10px'}}>Submit</Button>
       </Modal.Body>
+      <Modal.Footer style={{justifyContent:'center', textAlign:'center'}} >
+         <h5> All rights reserved to NIKE, I do not own any of the images or reference to this project</h5>
+      </Modal.Footer>
     </Modal>
   );
 }
